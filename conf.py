b17d72c598f5bc20bdea541792573f1836243a44
Chang Shuo <CS@x-134-84-73-119.uofm-secure.wireless.umn.edu>
2013-02-25 13:54:15 -0600

    changing the app id

--- conf.py
+++ conf.py
@@ -4,6 +4,6 @@ class Config(object):
     DEBUG = True
     TESTING = False
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
-    FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-    FBAPI_APP_SECRET = os.environ.get('FACEBOOK_SECRET')
+    FBAPI_APP_ID = '244705705666496' 
+    FBAPI_APP_SECRET ='b5e74b6ea15965fbda483535db50de5e' 
     FBAPI_SCOPE = ['user_likes', 'user_photos', 'user_photo_video_tags']
